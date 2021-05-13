1dac2c0b57c95beea15927df9bd045d319bd51f5
nagendra0032 <nagendrakumar0032@gmail.com>
2021-05-13 15:32:12 +0530

    minor changes

--- src/components/DataStructure/DataStructure.js
+++ src/components/DataStructure/DataStructure.js
@@ -3,10 +3,15 @@ import Resources from '../Resources/Resources';
 import styled from 'styled-components';
 import Card from '../Card/Card';
 import learnicon from '../../images/learnicon.png';
-import interviewicon from '../../images/interviewicon.png'
+import interviewicon from '../../images/interviewicon.png';
 
 const ResourceContainer = styled.div`
   background-color: #fafafa;
+  height: 91vh;
+
+  @media (max-width: 496px) {
+    height: auto; 
+  }
 `;
 
 const ResourceDetail = styled.div`
@@ -33,14 +38,14 @@ function DataStructure() {
         />
         <ResourceDetail>
           <Card
-            image = {learnicon}
+            image={learnicon}
             url="/"
             label="LEARN"
             description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod."
           />
           <Card
-            image = {interviewicon}
+            image={interviewicon}
             url="/"
             label="INTERVIEW PREPARATION"
             description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
